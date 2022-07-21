17b74546b41cd5cfb78de69e1634f66e50615d66
Matthew Guizar <matthewrguizar@gmail.com>
2022-07-21 07:39:46 -0700

    initial commit

---
+++ OperationChain.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class OperationChain {
+
+    static ArrayList<Double> prices = new ArrayList<Double>();
+
+
+    public static void main(String[] args) {
+        prices.add(1.99);
+        prices.add(4.99);
+        prices.add(10.99);
+        prices.add(15.99);
+
+        ArrayList<Double> updatePrices = new ArrayList<Double>();
+        updatePrices.addAll(prices.stream()
+            .filter((price) -> price < 5)
+            .map((price) -> price * 1.13)
+            .collect(Collectors.toList()));
+
+
+    }
+    
+
+}
